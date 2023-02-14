abcda2b024ac1837eed8142448b96e39b0a6c956
Gary H <3rdeye.ghj@gmail.com>
2023-02-14 14:12:32 -0800

    Figured out how to generate button values in the display

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const display = document.querySelector('.display');
 const keys = document.querySelector('#keys');
+const clear = document.querySelector('.clear')
 
-keys.addEventListener('click', e => {
+keys.addEventListener('click', event => {
+  const button = event.target;
+  if (!button.matches('button')) return;
+  const buttonValue = button.textContent; 
+  const displayValue = display.textContent; 
   
-})
+
+  if(displayValue === '0') {
+    display.textContent = buttonValue;
+  } else {
+    display.textContent += buttonValue; 
+  }
+});
