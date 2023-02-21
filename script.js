46c17f4bbf048d2789547bb974f7728a2c2ea27d
Gary H <3rdeye.ghj@gmail.com>
2023-02-21 14:01:45 -0800

    Working out test cases for continous calculations

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const equals = document.querySelector('.equal')
 let firstOperand = '';
 let secondOperand = '';
 let selectedOperator = '';
+let reset = false;
 
 keys.addEventListener('click', event => {
   const button = event.target;
@@ -16,8 +17,9 @@ keys.addEventListener('click', event => {
 
   if (button.classList.contains('operands')) {
     if (selectedOperator === '') {
-      if (display.textContent === '0') {
+      if (display.textContent === '0' || reset) {
         display.textContent = '';
+        reset = false;
       }
       firstOperand += buttonValue;
       display.textContent += buttonValue;
@@ -47,9 +49,10 @@ keys.addEventListener('click', event => {
         break;
     }
     display.textContent = result.toString();
-    firstOperand = result.toString();
+    firstOperand = '';
     secondOperand = '';
     selectedOperator = '';
+    reset = true;
   }
 });
 
@@ -61,5 +64,13 @@ clear.addEventListener('click', () => {
 });
 
 
+// Test Case 
+// Description: Perform continous calculation 1+1+1+1=4
 
+1.) Click 1+1
+EXPECTATION: Nothing. 1 displays with stored operator and opreands 
+ACTUAL: Works 
 
+1.) Click 1+1+1
+EXPECTATION: Nothing. 1 displays with stored operator and operands
+ACTUAL: Works 
