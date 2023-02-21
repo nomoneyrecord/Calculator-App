7bf359417c2fbed82756e316a396611503f8600e
Gary H <3rdeye.ghj@gmail.com>
2023-02-21 15:50:44 -0800

    Calculator is functioning
    Adding a decimal event

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const display = document.querySelector('.display');
 const keys = document.querySelector('#keys');
-const clear = document.querySelector('.clear')
-const operands = document.querySelector('.operands')
-const operators = document.querySelector('.operators')
-const decimal = document.querySelector('.decimal')
-const memory = document.querySelector('.memory')
-const equals = document.querySelector('.equal')
+const clear = document.querySelector('.clear');
+const operands = document.querySelector('.operands');
+const operators = document.querySelector('.operators');
+const decimal = document.querySelector('.decimal');
+const memory = document.querySelector('.memory');
+const equals = document.querySelector('.equal');
+
 let firstOperand = '';
 let secondOperand = '';
 let selectedOperator = '';
 let reset = false;
+let currentResult = null;
 
 keys.addEventListener('click', event => {
   const button = event.target;
@@ -28,31 +30,54 @@ keys.addEventListener('click', event => {
       display.textContent = secondOperand;
     }
   } else if (button.classList.contains('operators')) {
-    selectedOperator = buttonValue;
-    display.textContent = firstOperand;
-  } else if (button.classList.contains('equal')) {
-    const num1 = parseInt(firstOperand);
-    const num2 = parseInt(secondOperand);
-    let result;
-    switch (selectedOperator) {
-      case '+':
-        result = num1 + num2;
-        break;
-      case '-':
-        result = num1 - num2;
-        break;
-      case 'x':
-        result = num1 * num2;
-        break;
-      case '÷':
-        result = num1 / num2;
-        break;
+    if (currentResult === null) {
+      currentResult = parseFloat(firstOperand);
+    } else {
+      const num2 = parseFloat(secondOperand);
+      switch (selectedOperator) {
+        case '+':
+          currentResult += num2;
+          break;
+        case '-':
+          currentResult -= num2;
+          break;
+        case 'x':
+          currentResult *= num2;
+          break;
+        case '÷':
+          currentResult /= num2;
+          break;
+      }
+      display.textContent = currentResult.toString();
+      firstOperand = currentResult.toString();
+      secondOperand = '';
     }
-    display.textContent = result.toString();
-    firstOperand = '';
-    secondOperand = '';
-    selectedOperator = '';
+    selectedOperator = buttonValue;
     reset = true;
+  } else if (button.classList.contains('equal')) {
+    if (selectedOperator !== '' && secondOperand !== '') {
+      const num2 = parseFloat(secondOperand);
+      switch (selectedOperator) {
+        case '+':
+          result = currentResult + num2;
+          break;
+        case '-':
+          result = currentResult - num2;
+          break;
+        case 'x':
+          result = currentResult * num2;
+          break;
+        case '÷':
+          result = currentResult / num2;
+          break;
+      }
+      display.textContent = result.toString();
+      currentResult = null;
+      firstOperand = result.toString();
+      secondOperand = '';
+      selectedOperator = '';
+      reset = true;
+    }
   }
 });
 
@@ -60,17 +85,6 @@ clear.addEventListener('click', () => {
   firstOperand = '';
   secondOperand = '';
   selectedOperator = '';
+  currentResult = null;
   display.textContent = '0';
 });
-
-
-// Test Case 
-// Description: Perform continous calculation 1+1+1+1=4
-
-1.) Click 1+1
-EXPECTATION: Nothing. 1 displays with stored operator and opreands 
-ACTUAL: Works 
-
-1.) Click 1+1+1
-EXPECTATION: Nothing. 1 displays with stored operator and operands
-ACTUAL: Works 
